b39535bb880a77dbac6efbb9ada4df3626468c67
jaggernaute <lucas.videcoq1@gmail.com>
2022-11-11 23:47:22 +0100

    added dotenv file to store the token

--- src/main/java/fr/istic/bot/Main.java
+++ src/main/java/fr/istic/bot/Main.java
@@ -1,9 +1,32 @@
 package fr.istic.bot;
 
+import fr.istic.bot.core.CommandHandler;
+import fr.istic.bot.core.Listener;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_VOICE_STATES;
+
 public class Main {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws InterruptedException {
+        Dotenv dotenv = Dotenv.configure().load();
+        String token = dotenv.get("TOKEN");
+
+        JDA api = JDABuilder.create((token), GUILD_MESSAGES, GUILD_VOICE_STATES)
+                .addEventListeners(new Listener(new CommandHandler()))
+                .setActivity(Activity.playing("!help"))
+                .build();
+        api.awaitReady();
 
     }
-
 }
